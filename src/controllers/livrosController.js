855c034fb625c46ddd25e211e8053dba9414292a
Eduardo Raiol <marquesraiol@gmail.com>
2023-03-23 17:40:54 -0300

    Implementação do método POST

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -4,10 +4,16 @@ class LivroController {
 
     static listarLivros = (req, res) => {
         livros.find()
-            .then(livros => { res.status(200).json(livros)})
-            .catch(error => { console.log("Erro ao encontrar Livros", error)})
+            .then(livros => { res.status(200).send(livros)})
+            .catch(error => { res.status(500).send(`Erro ao encontrar Livros: ${error}`)})
     }
 
+    static cadatrarLivro = (req, res) => {
+        let livro = new livros(req.body);
+        livro.save()
+            .then(livro => { res.status(201).send(livro) })
+            .catch(error => { res.status(500).send(`Falha ao cadastrar o livro: ${error}`) } )
+    }
 }
 
 export default LivroController;
